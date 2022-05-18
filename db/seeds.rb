b9ad0e907ac2006e64dcbe38360ddb0f61d26e5d
Juris Veldre <jurisdavisveldre@gmail.com>
2022-05-18 10:39:27 +0300

    Add recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if Recipe.count.zero?
+  puts "Seeding recipes..."
+
+  path = File.join(File.dirname(__FILE__), "./data/recipes-en.json")
+  records = JSON.parse(File.read(path))
+
+  records.each do |record|
+    Recipe.create!(
+      title: record["title"],
+      cook_time: record["cook_time"],
+      prep_time: record["prep_time"],
+      ingredients: record["ingredients"],
+      ratings: record["ratings"],
+      cuisine: record["cuisine"],
+      category: record["category"],
+      author: record["author"],
+      image: record["image"]
+    )
+  end
+
+  puts "Recipes are seeded"
+end
